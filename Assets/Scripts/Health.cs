583d6b615e32a1a8800bbdf1c3d2296f9b531567
Greg Whitfield <greg.whitfield@datacom.co.nz>
2018-09-04 09:36:37 +1200

    Added network-synced health and respawn.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,23 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour {
+public class Health : NetworkBehaviour {
 
     public const int maxHealth = 100;
+	[SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
 
     public RectTransform healthBar;
   
     public void TakeDamage(int amount)
     {
+		if (!isServer) {
+			return;
+		}
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
-            currentHealth = 0;
-            Debug.Log("Dead!");
+            currentHealth = maxHealth;
+            // Called on the server, but invoked on the clients
+			RpcRespawn();
         }
-
-        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
     }
+
+	void OnChangeHealth (int currentHealth)
+	{
+		healthBar.sizeDelta = new Vector2 (currentHealth, healthBar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer) {
+			// move back to origin
+			transform.position = Vector3.zero;
+		}
+	}
 }
