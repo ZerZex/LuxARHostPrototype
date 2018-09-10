73a475c2a3fb359c3b85373092d4e32cfe281aec
Greg Whitfield [DATACOM] <Gregw@datacom.co.nz>
2018-09-10 17:34:07 +1200

    Added NPCs - non moving so far

---
+++ Assets/Scripts/AutoNPCSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class AutoNPCSpawner : NetworkBehaviour {
+	public GameObject autoNPCPrefab;
+	public int numberOfNPCs;
+
+	public override void OnStartServer()
+	{
+		for (int i = 0; i < numberOfNPCs; i++) {
+			var spawnPosition = new Vector3 (
+				                    Random.Range (-40.0f, 40.0f),
+				                    0.0f,
+				                    Random.Range (-40.0f, 40.0f));
+			var spawnRotation = Quaternion.Euler (
+				                    0.0f,
+				                    Random.Range (0, 180),
+				                    0.0f);
+			var npc = (GameObject.Instantiate (autoNPCPrefab, spawnPosition, spawnRotation));
+			NetworkServer.Spawn (npc);
+		}
+	}
+}
+
+
